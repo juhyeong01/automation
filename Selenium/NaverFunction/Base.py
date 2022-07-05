49bb4697dcdfd475edd7cbd0c120336745232f55
ShiNera01 <37740450+ShiNera01@users.noreply.github.com>
2022-07-05 22:27:04 +0900

    Create Base.py

---
+++ Selenium/NaverFunction/Base.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+import os
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()),options = options)
+driver.get('url')
+driver.maximize_window()
