fdfe7ffa1cddad4966a0c7cfecb9cc43c9b055a0
ShiNera01 <37740450+ShiNera01@users.noreply.github.com>
2022-07-06 21:25:43 +0900

    Update BaseFunction.py

--- Selenium/NaverFunction/SingleTest/Function/BaseFunction.py
+++ Selenium/NaverFunction/SingleTest/Function/BaseFunction.py
@@ -13,13 +13,32 @@ class BaseFunction:
     def get(self, url):
         
         self.driver.get(url)
+        
+
+    def send_keys(self, locator, value):
+
+        self.find_element(locator).send_keys(value)
 
 
-    def clic(self, locator):
+    def click(self, locator):
 
         try :
             WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator)).click()
 
+        except NoSuchElementException:
+            raise NoSuchElementException("NoSuchElementExcpetion : %s" str(locator))
+
+        except TimeoutException:
+            raise TimeoutException("TimeoutException")
+
+        
+
+    def find_element(self, locator):
+
+        try:
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+            return self.driver.find_element(locator)
+            
         except NoSuchElementException:
             raise NoSuchElementException("NoSuchElementExcpetion : %s" str(locator))
 
